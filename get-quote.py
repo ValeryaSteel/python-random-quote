1d8c702f740fc7de3875f8572aacf9fbe2567ac8
ValeryaSteel <69472156+ValeryaSteel@users.noreply.github.com>
2020-08-10 22:29:22 +0300

    Random quote bot is working
    Yeah, well. We're awfully Soviet here today, I see
    "Half measures are as bad as nothing at all"

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,17 @@
+import random
 def plop():
-   print("Keep it logically awesome.")
+   #print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+
+  last = 13
+  randomQuote = random.randint (0, last)
+  print(quotes[randomQuote])
+  lenQuotes = len(quotes)
+  print(lenQuotes)
 
 if __name__== "__main__":
   plop()
